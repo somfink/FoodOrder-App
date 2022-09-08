f69a8814c532d8d33077eb35961af0ac6498c025
Kacper Gajewski <kacper.gajewski@op.pl>
2022-09-08 12:19:07 +0200

    write logic to add items to cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,24 @@
+import { useContext } from "react";
+
+import CartContext from "../../store/cart-context";
 import { FaShoppingCart } from "react-icons/fa";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
-    <button className={classes.button}>
+
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <FaShoppingCart className={classes['icon-cart']}/>
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
